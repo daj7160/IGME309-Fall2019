f852b787c0315c3876bef3163f23e0c3d6925b9b
Darius James <daj7160@rit.edu>
2019-10-06 20:22:55 -0400

    E08 work

--- E08 - Projections/AppClass.cpp
+++ E08 - Projections/AppClass.cpp
@@ -3,7 +3,7 @@ using namespace Simplex;
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Darius James - daj7160@rit.edu";
 
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -54,12 +54,17 @@ void Application::Display(void)
 		break;
 	case 2:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPerspective(false);
+		m_pCamera->SetPositionTargetAndUpward(vector3(10.0, -60.0, 500.0), vector3(0.0, 0.0, 0.0), vector3(0.0, 0.0, 1.0));
+		
 		break;
 	case 3:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(22.5, 0.0, 0.0), vector3(0.0, 0.0, 0.0), vector3(0.0, 0.0, -1.0));
 		break;
 	case 4:
 		m_pCamera->ResetCamera();
+		m_pCamera->SetPositionTargetAndUpward(vector3(0.0, -5.0, -90.0), vector3(0.0, 0.0, -10.0), vector3(0.0, 0.0, -1.0));
 		break;
 	case 5:
 		m_pCamera->ResetCamera();
